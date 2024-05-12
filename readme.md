0ef48194cf8c79c4202b81ddc3ac23db1c8e18f5
jwe0 <jwe0@fbi.gov>
2024-05-12 11:54:50 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Rust Hash Cracker
+> *Speedy*
+
+Hash cracker written in rust because I was bored.
+
+### Features
+- Threading to allow multiple attempets at the same time
+- 2 Hashing methods (Wow)
+- Easy to use (Insane)
+
+
+### Methods
+
+| Syntax      | Description |
+| ----------- | ----------- |
+| MD5         | ✅          |
+| SHA256      | ✅          |
+| Other sha   | 🔜          |
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::io::{self, Write, BufRead, BufReader};
+use std::fs::File;
+use std::process::exit;
+use std::{thread, time::Instant};
+use sha2::{Digest, Sha256};
+use md5;
+
+
+
+fn art() {
+    println!(r#"
+    ____             __     ______                __            
+   / __ \__  _______/ /_   / ____/________ ______/ /_____  _____
+  / /_/ / / / / ___/ __/  / /   / ___/ __ `/ ___/ //_/ _ \/ ___/
+ / _, _/ /_/ (__  ) /_   / /___/ /  / /_/ / /__/ ,< /  __/ /    
+/_/ |_|\__,_/____/\__/   \____/_/   \__,_/\___/_/|_|\___/_/     
+
+"#)
+}
+
+
+
+fn md5_method(hash: &str, word: &str, time: u64) {
+    let digest = md5::compute(word.as_bytes());        
+    let digest_str = format!("{:x}", digest);
+    if digest_str == hash.to_string() {
+        println!("[!] Cracked password: {}\n[!] Took: {}s", word, time);
+        
+        exit(0);
+    }
+}
+
+fn sha256_method(hash: &str, word: &str, time: u64) {
+    let mut hasher = Sha256::new();
+    hasher.update(word.as_bytes());
+    let result = hasher.finalize();
+    let hash_result = format!("{:x}", result);
+
+    if hash_result == hash.to_string() {
+        println!("[!] Cracked password: {}\n[!] Took: {}s", word, time);
+        exit(0);
+    }
+}
+
+
+fn main() {
+    art();
+    print!("[?] Hash: ");
+    io::stdout().flush().unwrap();
+    let mut hash = String::new();
+    io::stdin().read_line(&mut hash).unwrap();
+    let hash = hash.trim();
+
+    print!("[?] Wordlist: ");
+    io::stdout().flush().unwrap();
+    let mut wordlist = String::new();
+    io::stdin().read_line(&mut wordlist).unwrap();
+    let wordlist = wordlist.trim();
+
+
+    print!("[?] Algorithm: ");
+    io::stdout().flush().unwrap();
+    let mut algorithm = String::new();
+    io::stdin().read_line(&mut algorithm).unwrap(); 
+    let algorithm = algorithm.trim();
+
+    let file = File::open(wordlist).unwrap();
+    let reader = BufReader::new(file);
+
+    let start = Instant::now();
+
+    println!("\n[>] Begun cracking\n");
+
+    for line in reader.lines() {
+        let algorithm2 = algorithm.to_string();
+        let hash2 = hash.to_string();
+        thread::spawn(move || {
+            let word = line.unwrap();
+            if algorithm2 == "md5" {
+                md5_method(hash2.as_str(), word.as_str(), start.elapsed().as_secs());
+            } else if algorithm2 == "sha256" {
+                sha256_method(hash2.as_str(), word.as_str(), start.elapsed().as_secs());
+            }
+        });
+    }
+    println!("[!] Failed to crack password");
+        
+}
+
+
